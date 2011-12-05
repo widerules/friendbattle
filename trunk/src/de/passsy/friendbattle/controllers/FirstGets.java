c02b9b045456c9eb84fa368602a91e0c8802c219
pascal.welsch <pascal.welsch@c5b0c20b-37c1-311f-b25f-b5c061d5e27e>
2011-12-05 16:18:47 +0000

    Added PointProvider to declare the different gametypes

---
+++ trunk/src/de/passsy/friendbattle/controllers/FirstGets.java
@@ -0,0 +1,53 @@
+package de.passsy.friendbattle.controllers;
+
+import de.passsy.friendbattle.data.Player;
+import de.passsy.friendbattle.games.MiniGame;
+import de.passsy.friendbattle.games.MiniGame.Correctness;
+import de.passsy.friendbattle.utility.GoodTimer;
+import de.passsy.friendbattle.utility.GoodTimer.OnTimerListener;
+
+public class FirstGets extends PointProvider {
+
+    private Boolean mSolved = false;
+
+    @Override
+    public Correctness evalCorrectness(Boolean correctness, Player player,
+	    final MiniGame miniGame) {
+	Correctness result = Correctness.incorrect;
+	if (correctness) {
+	    // Game is correct
+	    if (!mSolved) {
+		// First who guessed correct
+		// Points++
+		result = Correctness.correct;
+	    } else {
+		// Player was to late, someone else was faster
+		// Points don't change
+		result = Correctness.toolate;
+	    }
+	    // now no one can get points
+	    mSolved = true;
+	    // start next game in 1 second
+
+	    GoodTimer timer = new GoodTimer(1000, false);
+	    timer.setOnTimerListener(new OnTimerListener() {
+
+		@Override
+		public void onTimer() {
+		    miniGame.getOnNextGameListener().onNextGame(miniGame);
+		}
+	    });
+	    timer.start();
+
+	} else {
+	    // Game isn't correct
+	    if (!mSolved) {
+		// Player gets Points--
+		result = Correctness.incorrect;
+	    }
+	}
+	return result;
+
+    }
+
+}
